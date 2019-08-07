e26975ca69b13f4deb8b94402dcd9009f9b830b2
Graham Fairweather <xotic750@gmail.com>
2019-08-07 03:22:04 +0200

    Auto build

--- dist/array-all-x.js
+++ dist/array-all-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-06T21:09:12.263Z",
+  "date": "2019-08-07T01:21:58.082Z",
   "describe": "",
   "description": "Like Array.forEach but does not skip holes.",
   "file": "array-all-x.js",
