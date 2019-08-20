fa6298401d9a55a93e248f8d0847a2cedff28532
Graham Fairweather <xotic750@gmail.com>
2019-08-20 11:26:08 +0200

    Maintenance

--- dist/array-all-x.js
+++ dist/array-all-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-18T14:09:39.538Z",
+  "date": "2019-08-20T09:26:01.656Z",
   "describe": "",
   "description": "Like Array.forEach but does not skip holes.",
   "file": "array-all-x.js",
